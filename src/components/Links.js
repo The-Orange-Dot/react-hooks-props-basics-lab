ace1b7642ae1c07ab9725807fab1b19c58ddd524
TheOrangeDot <theorangedottwitch@gmail.com>
2021-12-13 13:42:15 +0900

    Completed

---
+++ src/components/Links.js
@@ -0,0 +1,9 @@
+const Links = (props) => (
+  <div>
+    <h3>Links</h3>
+    <a href={props.github}>https://github.com/liza</a>
+    <a href={props.linkedin}>https://www.linkedin.com/in/liza/</a>
+  </div>
+);
+
+export default Links;
